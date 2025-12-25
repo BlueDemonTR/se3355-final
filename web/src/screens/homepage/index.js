f67ed31a87c161967b97e07b499537c4c3be7ef5
Poyopoyoraz <83763146+BlueDemonTR@users.noreply.github.com>
2025-12-25 17:51:18 +0400

    update styling

--- web/src/screens/homepage/index.js
+++ web/src/screens/homepage/index.js
@@ -6,7 +6,12 @@ const Homepage = ({  }) => {
   return (
     <ContentArea>
       <Text>
-        Since I don't currently have an SSL Certificate, for the browser to be able to connect to the server properly, you may first need to go to {process.env.REACT_APP_API_URL} then press "Accept Risk and Continue" from the settings. I will try to fix this as soon as possible, sorry for the inconvenience.
+        Since I don't currently have an SSL Certificate, for the browser to be able to connect to the server properly, you may first need to go to 
+        {' '}<a 
+          className='text-blue-700'
+          href={process.env.REACT_APP_API_URL}
+        >This Link</a>{' '}
+        then press "Accept Risk and Continue" from the settings. I will try to fix this as soon as possible, sorry for the inconvenience.
       </Text>
     </ContentArea>
   )
